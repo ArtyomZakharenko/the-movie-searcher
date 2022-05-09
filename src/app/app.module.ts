cb2f02cc3d3d376d59c6186e38dc13e5f24cd791
Artyom Zakharenko <artiomzaharenko@gmail.com>
2022-05-09 14:11:02 +0300

    added Movies List component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { MoviesListComponent } from './movies-list/movies-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    MoviesListComponent
   ],
   imports: [
     BrowserModule,
